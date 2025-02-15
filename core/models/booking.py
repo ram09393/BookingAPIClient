56cf51ebfb5eaa00dcfd134a29e28a9a92ef4a98
RAMISK <rkomiakov@mail.ru>
2025-02-16 00:58:22 +0300

    Добавил файл booking.py

---
+++ core/models/booking.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import date
+
+
+class BookingDates(BaseModel):
+    checkin: date
+    checkout: date
+
+
+class Booking(BaseModel):
+    firstname: str
+    lastname: str
+    totalprice: int
+    depositpaid: bool
+    bookingdates: BookingDates
+    additionalneeds: Optional[str] = None
+
+
+class BookingResponse(BaseModel):
+    bookingid: int
+    booking: Booking
+
+
+#тест коммита
